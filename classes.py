e39f8eecd08ae57b286936a8b4611421ef0291cf
donnamagi <magi.donna@gmail.com>
2022-03-14 18:13:47 +0100

    Created registering form

--- classes.py
+++ classes.py
@@ -5,3 +5,9 @@ from wtforms.validators import DataRequired, EqualTo
 class SubmitForm(FlaskForm):
     todo = StringField('Todo:', validators= [DataRequired()])
     submit = SubmitField('Add task')
+
+class RegisterForm(FlaskForm):
+    username = StringField('username', validators = [DataRequired()])
+    password = PasswordField('password', validators = [DataRequired()])
+    password2 = PasswordField('repeat password', validators = [DataRequired(), EqualTo('password', message= 'Passwords must match.')])
+    submit = SubmitField ('Register')
