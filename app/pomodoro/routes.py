4f0f909c22a6d094d4b2c312c515e31b242a9cce
donnamagi <magi.donna@gmail.com>
2022-03-17 12:28:18 +0100

    Creating blueprints

---
+++ app/pomodoro/routes.py
@@ -0,0 +1,28 @@
+from flask import Blueprint, Flask, render_template, url_for, flash, redirect
+from app.classes import SubmitForm
+
+blueprint = Blueprint('pomodoro', __name__)
+
+
+todos = []
+
+@blueprint.route('/todolist', methods = ['GET', 'POST'])
+def todolist():
+
+    form = SubmitForm()
+    
+    if form.validate_on_submit():
+        todos.append(form.todo.data)
+        flash('Task added!')
+
+
+    return render_template('todolist.html', title = 'Todo list', form = form, todos = todos)
+
+
+@blueprint.route('/delete/<task>')
+def delete(task):
+
+    todos.remove(task)
+    
+    return redirect(url_for('todolist')), flash("Entry deleted")
+
