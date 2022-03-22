5bb0786ba8b562fe195b71ca6f5331520f244ddb
donnamagi <magi.donna@gmail.com>
2022-03-22 09:57:32 +0200

    New blueprint folder

---
+++ app/main_pages/routes.py
@@ -0,0 +1,20 @@
+from flask import Blueprint, render_template, url_for, redirect
+
+blueprint = Blueprint('main_pages', __name__)
+
+##################
+
+@blueprint.route('/', methods = ['GET', 'POST'])
+def index():
+    return render_template('index.html', title = 'Pomodoro app')
+
+@blueprint.route('/login', methods= ['GET', 'POST'])
+def login():
+    return render_template('login.html', title = 'Login')
+
+# For future reference
+
+# @blueprint.route('/logout')
+# def logout():
+#     logout_user()
+#     return redirect(url_for('main_pages.login'))
