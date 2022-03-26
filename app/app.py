8ca173976bffe292d128a5fd66ed0e22a9ecddba
donnamagi <magi.donna@gmail.com>
2022-03-26 11:36:59 +0200

    DB initialisation

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from . import pomodoro, main_pages
 from log import logger
+from app.extensions.database import db
 
 def page_not_found(e):
   logger.error('Page not found')
@@ -10,6 +11,9 @@ def register_blueprints(app: Flask):
     app.register_blueprint(pomodoro.routes.blueprint)
     app.register_blueprint(main_pages.routes.blueprint)
 
+def register_extensions(app: Flask):
+  db.init_app(app)
+
 def create_app():
   app = Flask(__name__)
 
@@ -18,6 +22,7 @@ def create_app():
   app.config.from_object('app.config')
 
   #Routes
+  register_extensions(app)
   register_blueprints(app)
   app.register_error_handler(404, page_not_found)
 
--- app/main_pages/models.py
+++ app/main_pages/models.py
@@ -1,5 +1,13 @@
-class User():
-    id= ''
-    username = ''
-    password = ''
-    
+from app.extensions.database import db
+
+class User(UserMixin, db.Model):
+    id= db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(100), index = True, unique = True)
+    password = db.Column(db.String(100))
+
+class Todos(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    task = db.Column(db.String(200), index = True)
+
+
