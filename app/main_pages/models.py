c53a7f7d6a224bf0cd4a59c5b2ee3d95c670be82
donnamagi <magi.donna@gmail.com>
2022-03-30 17:57:26 +0300

    Password hashing

--- app/main_pages/models.py
+++ app/main_pages/models.py
@@ -1,10 +1,17 @@
 from app.extensions.database import db
+from werkzeug.security import check_password_hash, generate_password_hash
 
 class User(db.Model):
     id= db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(100), index = True, unique = True)
     password = db.Column(db.String(100))
     todos = db.relationship('Todos', backref = 'todos', lazy = True)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
 
 class Todos(db.Model):
     id = db.Column(db.Integer, primary_key = True)
