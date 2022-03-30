f94551bd30baeaabaa3088e57041dc6d1f1edd94
donnamagi <magi.donna@gmail.com>
2022-03-30 17:21:15 +0300

    Creating register route

--- app/main_pages/routes.py
+++ app/main_pages/routes.py
@@ -13,6 +13,32 @@ def index():
 def login():
     return render_template('login.html', title = 'Login')
 
+@blueprint.route('/register', methods= ['GET', 'POST'])
+def register():
+    form = RegisterForm()
+
+    if form.password.data != form.password2.data:
+        flash('Make sure you enter the same password')
+
+    if form.validate_on_submit():
+        
+        user = User(username = form.username.data)
+        user.set_password(form.password.data)
+
+        try:
+            db.session.add(user)
+            db.session.commit()
+            # login_user(user)
+            return redirect(url_for('main_pages.index'))
+        except:
+            db.session.rollback()
+            
+        if not User.query.get(form.email.data):
+            flash('This email is already in use.')
+
+        
+    return render_template('registration.html', form = form, title = 'Register')
+
 # For future reference
 
 # @blueprint.route('/logout')
