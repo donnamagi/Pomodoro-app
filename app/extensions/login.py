e8d4e6c8ac4bcd570b64c0d743b53fc2498d910a
donnamagi <magi.donna@gmail.com>
2022-03-30 23:15:32 +0300

    Bug fixes

--- app/extensions/login.py
+++ app/extensions/login.py
@@ -1,7 +1,8 @@
 from flask_login import LoginManager
+from app.main_pages.models import User
 
 login_manager = LoginManager()
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.get(user_id)
+    return User.query.get(user_id)
