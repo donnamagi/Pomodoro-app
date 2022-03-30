5469caee081a1d4873a8d6e24229da136b8b25d1
donnamagi <magi.donna@gmail.com>
2022-03-30 10:23:55 +0300

    Testing with db updates

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,10 +1,13 @@
 import pytest
 from app.app import create_app
+from flask_migrate import upgrade
 
 @pytest.fixture
 def client():
     app = create_app()
 
     with app.app_context():
+        upgrade()
         yield app.test_client()
+
 
