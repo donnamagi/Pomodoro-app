9accba8cb12790c6ba4978d3a9dcc738e006707c
donnamagi <magi.donna@gmail.com>
2022-04-02 09:47:37 +0200

    Database implementation to adding tasks

--- app/pomodoro/routes.py
+++ app/pomodoro/routes.py
@@ -1,35 +1,33 @@
 from flask import Blueprint, Flask, render_template, url_for, flash, redirect
 from app import classes
 from app.extensions.database import db
-from flask_login import current_user
+from flask_login import current_user, login_required
 from app.main_pages.models import Todos
 
 blueprint = Blueprint('pomodoro', __name__)
 
 
-todos = []
 
 @blueprint.route('/todolist', methods = ['GET', 'POST'])
+@login_required
 def todolist():
-    global todos
-    form = classes.SubmitForm()
 
+    id = current_user.id
+    todos = current_user.todos.all()
+    form = classes.SubmitForm()
+    
     if form.validate_on_submit():
 
-        if current_user.is_authenticated:
-            id = current_user.id
-            tasks = Todos.query.filter_by(user_id = id).all()     
-            task = Todos(task = form.todo.data, user_id = id)
-            try:
-                db.session.add(task)
-                db.session.commit()  
-                flash('Task added!')
-            except:
-                db.session.rollback()
-                flash('Error')
-        else:
-            todos.append(form.todo.data)
+        task = Todos(task = form.todo.data, user_id = id)
+
+        try:
+            db.session.add(task)
+            db.session.commit()  
             flash('Task added!')
+            return redirect(url_for('pomodoro.todolist'))
+        except:
+            db.session.rollback()
+            flash('Error')
 
     return render_template('todolist.html', title = 'Todo list', form = form, todos = todos)
 
