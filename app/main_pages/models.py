496a1f9bfee1172c993aaa0b61e91c7e741e24d2
donnamagi <magi.donna@gmail.com>
2022-04-03 13:39:35 +0200

    Nope

--- app/main_pages/models.py
+++ app/main_pages/models.py
@@ -2,7 +2,7 @@ from app.extensions.database import db
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask_login import UserMixin
 
-class user(db.Model, UserMixin):
+class User(db.Model, UserMixin):
     id= db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(100), index = True, unique = True)
     password = db.Column(db.String(100))
@@ -14,7 +14,7 @@ class user(db.Model, UserMixin):
     def check_password(self, password):
         return check_password_hash(self.password, password)
 
-class todos(db.Model):
+class Todos(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     task = db.Column(db.String(200), index = True)
