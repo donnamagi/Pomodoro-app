21ef9a6f76d08448b808b10672761537d130f960
donnamagi <magi.donna@gmail.com>
2022-04-06 15:57:58 +0200

    New db migration

---
+++ migrations/versions/749e9969cad1_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 749e9969cad1
+Revises: 031768961841
+Create Date: 2022-04-06 15:54:28.060365
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '749e9969cad1'
+down_revision = '031768961841'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
