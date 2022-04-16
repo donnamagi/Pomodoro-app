c2ae50878377cb5d1b34c34fd5d6f457b5ed2302
donnamagi <magi.donna@gmail.com>
2022-04-16 16:55:59 +0200

    Cleanup

--- app/static/src/timer.js
+++ app/static/src/timer.js
@@ -1,24 +1,19 @@
 class Timer {
     constructor(minutes) {
-        this.remainingTime = minutes * 60 * 1000;
-        this.interval;
+        this.remainingTime = this.minToMs(minutes);
         this.isPaused = false;
         this.continue = true;
         setInterval(this.runTimer.bind(this), 1000)
     };
 
-    setTime(minutes) {
-        this.remainingTime = minutes * 60 * 1000;
-    }
-    
     startTimer() {
         this.isPaused = false;
     }
-
+    
     pauseTimer() {
         this.isPaused = true; 
-     } 
-
+    } 
+    
     runTimer() {
         if (!this.isPaused) {
             this.remainingTime = this.remainingTime - 1000;
@@ -28,6 +23,10 @@ class Timer {
         }
     }
 
+    minToMs(minutes) {
+        return minutes * 60 * 1000;
+    }
+    
     msCalculator(ms) {
         const date = new Date(ms);
         return `${('00'+date.getMinutes()).slice(-2)}:${('00'+date.getSeconds()).slice(-2)}`
