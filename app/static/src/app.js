66ae7d2542f12431911508d89469c9f8a0392aa1
donnamagi <magi.donna@gmail.com>
2022-04-16 16:39:33 +0200

    Working timer in console

--- app/static/src/app.js
+++ app/static/src/app.js
@@ -1,83 +1,92 @@
-let counterWorkElem = document.querySelector('.work');
-let counterRestElem = document.querySelector('.rest');
-let counterBreakElem = document.querySelector('.break');
-let counterDisplayElem = document.querySelector('.counter-display');
-let counterSkipElem = document.querySelector('.counter-skip');
-let counterStartElem = document.querySelector('.counter-start');
+const timer = new Timer(25);
 
-// Default variables for when the todos page loads
-let counterMinute = 25;
-let counterSecond = 0;
-let isPaused = true;
-let currentButton = document.getElementById('work');
-currentButton.style.backgroundColor = "rgb(171, 169, 169)";
+timer.startTimer();
 
-function currentlyActive(selected){
-    currentButton.style.backgroundColor = "inherit";     // Removes previous highlight
-    currentButton = document.getElementById(selected);   // Reassigns active button
-    currentButton.style.backgroundColor = "rgb(171, 169, 169)";
-}
+// let counterWorkElem = document.querySelector('.work');
+// let counterRestElem = document.querySelector('.rest');
+// let counterBreakElem = document.querySelector('.break');
+// let counterDisplayElem = document.querySelector('.counter-display');
+// let counterSkipElem = document.querySelector('.counter-skip');
+// let counterStartElem = document.querySelector('.counter-start');
 
-const fillZero=(n)=> ('00'+n).slice(-2);
-updateDisplay();
+// // Default variables for when the todos page loads
+// let counterMinute = 25;
+// let counterSecond = 0;
+// let isPaused = true;
+// let currentButton = document.getElementById('work');
+// currentButton.style.backgroundColor = "rgb(171, 169, 169)";
 
-function updateDisplay(){
-    counterDisplayElem.innerHTML = fillZero(counterMinute) + ':' + fillZero(counterSecond);
-};
+// function currentlyActive(selected){
+//     currentButton.style.backgroundColor = "inherit";     // Removes previous highlight
+//     currentButton = document.getElementById(selected);   // Reassigns active button
+//     currentButton.style.backgroundColor = "rgb(171, 169, 169)";
+// }
 
-// Event listeners
+// const fillZero=(n)=> ('00'+n).slice(-2);
+// updateDisplay();
 
-counterSkipElem.addEventListener("click",()=>{
-    clearInterval(PomodoroCounter);
-    updateDisplay();
-});
+// function updateDisplay(){
+//     counterDisplayElem.innerHTML = fillZero(counterMinute) + ':' + fillZero(counterSecond);
+// };
 
-counterWorkElem.addEventListener("click", ()=>{
-    counterMinute = 25;
-    counterSecond = 0;
-    updateDisplay();
-    currentlyActive(counterWorkElem.id);
-});
+// // Event listeners
 
-counterRestElem.addEventListener("click", ()=>{
-    counterMinute = 5;
-    counterSecond = 0;
-    updateDisplay();
-    currentlyActive(counterRestElem.id);
-});
+// counterSkipElem.addEventListener("click",()=>{
+//     var orderOfButtons = ['work', 'rest', 'break', 'work'];
+//     var index = orderOfButtons.indexOf(currentButton.id);
+//     var redirect = orderOfButtons[index + 1];
 
-counterBreakElem.addEventListener("click", ()=>{
-    counterMinute = 15;
-    counterSecond = 0;
-    updateDisplay();
-    currentlyActive(counterBreakElem.id);
-});
 
-counterStartElem.addEventListener("click", () => {
-    if (isPaused) {
-        isPaused = false;
-        counterStartElem.innerHTML = 'Pause';
-    } else {
-        isPaused = true;
-        counterStartElem.innerHTML = 'Start';
-    }
-    updateDisplay();
-})
+//     clearInterval(PomodoroCounter);
+//     updateDisplay();
+// });
 
-PomodoroCounter = setInterval(timer, 1000);
+// counterWorkElem.addEventListener("click", ()=>{
+//     counterMinute = 25;
+//     counterSecond = 0;
+//     updateDisplay();
+//     currentlyActive(counterWorkElem.id);
+// });
 
-function timer() {
-    if (!isPaused) {
-        if (counterSecond == 0) {
-            counterMinute--;
-            counterSecond = 60;
-            updateDisplay();
-        } 
-        if (counterMinute <= 1) {
-            clearInterval(PomodoroCounter);
-        }
+// counterRestElem.addEventListener("click", ()=>{
+//     counterMinute = 5;
+//     counterSecond = 0;
+//     updateDisplay();
+//     currentlyActive(counterRestElem.id);
+// });
+
+// counterBreakElem.addEventListener("click", ()=>{
+//     counterMinute = 15;
+//     counterSecond = 0;
+//     updateDisplay();
+//     currentlyActive(counterBreakElem.id);
+// });
+
+// counterStartElem.addEventListener("click", () => {
+//     if (isPaused) {
+//         isPaused = false;
+//         counterStartElem.innerHTML = 'Pause';
+//     } else {
+//         isPaused = true;
+//         counterStartElem.innerHTML = 'Start';
+//     }
+//     updateDisplay();
+// })
+
+// PomodoroCounter = setInterval(timer, 1000);
+
+// function timer() {
+//     if (!isPaused) {
+//         if (counterSecond == 0) {
+//             counterMinute--;
+//             counterSecond = 60;
+//             updateDisplay();
+//         } 
+//         if (counterMinute <= 1) {
+//             clearInterval(PomodoroCounter);
+//         }
     
-        counterSecond--;
-        updateDisplay();
-    }
-    }
+//         counterSecond--;
+//         updateDisplay();
+//     }
+//     }
--- app/static/src/timer.js
+++ app/static/src/timer.js
@@ -9,6 +9,19 @@ class Timer {
     setTime(minutes) {
         this.remainingTime = minutes * 60 * 1000;
     }
+    
+    startTimer() {
+        setInterval(this.runTimer.bind(this), 1000);
+    }
+
+    runTimer() {
+        if (!this.isPaused) {
+            this.remainingTime = this.remainingTime - 1000;
+            var time = this.msCalculator(this.remainingTime);
+            console.log(time);
+            // updateDisplay();
+        }
+    }
 
     msCalculator(ms) {
         const date = new Date(ms);
