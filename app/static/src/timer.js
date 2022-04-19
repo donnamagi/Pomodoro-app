1bb4c58f528928ed202d95d007baaab526e73e2c
donnamagi <magi.donna@gmail.com>
2022-04-19 09:45:31 +0200

    Working logic for pomodoro sequence

--- app/static/src/timer.js
+++ app/static/src/timer.js
@@ -4,8 +4,10 @@ class Timer {
         this.isPaused = true;
         this.continue = true;
         this.display = display;
+        this.pomodoro = 3;
+        this.session = 25;
         this.updateDisplay();
-        setInterval(this.runTimer.bind(this), 1000);
+        this.interval = setInterval(this.runTimer.bind(this), 1);
     };
 
     startTimer() {
@@ -25,7 +27,42 @@ class Timer {
         if (!this.isPaused) {
             this.remainingTime = this.remainingTime - 1000;
             this.updateDisplay();
-        }  
+        };
+        if (this.remainingTime == 0) {
+            clearInterval(this.interval);
+            this.newInterval();
+        }
+    }
+    
+    newInterval() {
+        var next = this.timerSequence();
+        this.newTimer(next);
+        this.interval = setInterval(this.runTimer.bind(this), 1);
+        this.updateDisplay();
+    }
+
+    newTimer(minutes) {
+        this.session = minutes;
+        if (!this.isPaused) {
+            this.pauseTimer();
+        };
+        this.remainingTime = this.minToMs(minutes);
+    }
+
+    timerSequence() {
+        switch (this.session) {
+            case 25:
+                this.pomodoro--;
+                if (this.pomodoro = 0) {
+                    this.pomodoro = 3;
+                    return 15;                    
+                }
+                return 5;
+            case 5:
+                return 25;
+            case 15:
+                return 25;
+        };
     }
 
     minToMs(minutes) {
@@ -35,14 +72,6 @@ class Timer {
     msCalculator(ms) {
         const date = new Date(ms);
         return `${('00'+date.getMinutes()).slice(-2)}:${('00'+date.getSeconds()).slice(-2)}`
-    }
-
-    newTimer(minutes) {
-        if (!this.isPaused) {
-            this.pauseTimer();
-        };
-        this.remainingTime = this.minToMs(minutes);
-        this.updateDisplay();
     }
 }
 
