b4ac84f1b6bd5ea8e15cd6fb7a597b0be6f8bc6f
donnamagi <magi.donna@gmail.com>
2022-04-22 11:36:38 +0200

    Test coverage for all routes

--- app/tests/main_pages/test_main.py
+++ app/tests/main_pages/test_main.py
@@ -1,3 +1,5 @@
+from flask_login import current_user, login_user
+
 def test_index_success(client):
     #Page loads
     response = client.get('main_pages.index')
@@ -6,4 +8,14 @@ def test_index_success(client):
 def test_login_success(client):
     #Page loads
     response = client.get('main_pages.login')
+    assert response.status_code == 404
+
+def test_register_success(client):
+    #Page loads
+    response = client.get('main_pages.register')
+    assert response.status_code == 404
+
+def test_logout_success(client):
+    #Page loads
+    response = client.get('main_pages.logout')
     assert response.status_code == 404
--- app/tests/pomodoro/test_routes.py
+++ app/tests/pomodoro/test_routes.py
@@ -1,4 +1,12 @@
+from app.main_pages import models
+
+
 def test_todolist_success(client):
     #Page loads
     response = client.get('pomodoro.todolist')
+    assert response.status_code == 404
+
+def test_deletion_success(client):
+    #Page loads
+    response = client.get('pomodoro.delete')
     assert response.status_code == 404
